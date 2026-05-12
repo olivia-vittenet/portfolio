3edee787b9d5222381ab1ff2c1b76aa5cb311f4d
Olivia Vittenet <olivia.vittenet@isen.yncrea.fr>
2026-05-12 19:59:07 +0200

    initialize next.js project with typescript and quality tools

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
+}
+
+export default nextConfig
